8478168a6d50635444a451e789b9265dd61aef26
chihyu2790 <aaa2790aaa@gmail.com>
2022-06-07 15:50:54 +0800

    Add files

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<!-- <script>
+import TopImage from '@/components/TopImage.vue'
+export default {
+  components: {
+    TopImage
+  }
+}
+</script> -->
+
+<script setup>
+import TopImage from '@/components/TopImage.vue'
+import LeftMenu from '@/components/LeftMenu.vue'
+import LeftUpdate from '@/components/LeftUpdate.vue'
+import RightMarquee from '@/components/RightMarquee.vue'
+import RightMain from '@/components/RightMain.vue'
+</script>
+
+<template lang="pug">
+TopImage
+#center
+  #left
+    LeftMenu
+    LeftUpdate
+  #right
+    RightMarquee
+    RightMain
+</template>
